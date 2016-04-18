6d0d33d10642c0d31e8bc07cb60bfa99f95ed039
mkampasi <manishakampasi22@gmail.com>
2016-04-18 00:14:19 -0700

    Updated with annaphora selection and looking back multiple lines

--- 2.CandidateSelection/CandidateSelection/candidateSelection.py
+++ 2.CandidateSelection/CandidateSelection/candidateSelection.py
@@ -1,67 +1,73 @@
+#! /usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 import codecs
-from collections import defaultdict,Counter
+import os
 from trainStoryProcessor import createDict
-
-rootDir=dict()
-stop_list=[]
-
-def isAnaphora(word,POS,stop_list):
-    if POS == "PRP" and word not in stop_list:
-        return True
-    return False
 
 def candidateSelection(rootDir,word,sentenceNum,gender):
     possibleCandidates=[]
-
+    previousLine = {}
     if sentenceNum > 0:
-        previousLine = rootDir[sentenceNum - 1]
+        previousLine["0"] = rootDir[sentenceNum]
+        previousLine["1"]=rootDir[sentenceNum - 1]
         if gender=="m" or gender=="f" or gender=="n" or gender=="fn" :
-            for wordDict in previousLine:
-                if len(rootDir[sentenceNum-1][wordDict]) > 5:
-                    if ( ( rootDir[sentenceNum-1][wordDict][5] == gender) and (rootDir[sentenceNum-1][wordDict][1] == "NN" or rootDir[sentenceNum-1][wordDict][1]=="NNP") ):
-                        possibleCandidates.append(wordDict)
+            for linevalue in previousLine.itervalues():
+                for wordDict in linevalue.itervalues():
+                    if len(wordDict) > 9:
+                        if ( ( wordDict[7] == gender) and (wordDict[1] == "NN" or wordDict[1]=="NNP") ):
+                            possibleCandidates.append(wordDict)
         elif gender=="mf":
-            for wordDict in previousLine:
-                if len(rootDir[sentenceNum - 1][wordDict]) > 5:
-                    currentWordGender = rootDir[sentenceNum][wordDict][5]
-                    if( (currentWordGender == "m" or currentWordGender == "f" or currentWordGender == "mf") and (rootDir[sentenceNum-1][wordDict][1] == "NN" or rootDir[sentenceNum-1][wordDict][1]=="NNP") ):
-                        possibleCandidates.append(wordDict)
-        elif gender=="any":
-            for wordDict in previousLine:
-                if ( rootDir[sentenceNum-1][wordDict][1] == "NN" or rootDir[sentenceNum-1][wordDict][1]=="NNP" ):
-                    if len(rootDir[sentenceNum - 1][wordDict]) > 5:
-                        possibleCandidates.append(wordDict)
-
+            for linevalue in previousLine.itervalues():
+                for wordDict in linevalue.itervalues():
+                    if len(wordDict) > 9:
+                        currentWordGender = wordDict[7]
+                        if( (currentWordGender == "m" or currentWordGender == "f" or currentWordGender == "mf") and (wordDict[1] or wordDict[1]=="NNP") ):
+                            possibleCandidates.append(wordDict)
+        else:
+            for linevalue in previousLine.itervalues():
+                for wordDict in linevalue.itervalues():
+                    if( wordDict[1] == "NN" or wordDict[1]=="NNP" ):
+                        if len(wordDict) > 9:
+                            possibleCandidates.append(wordDict)
+            
         return possibleCandidates
+    else:
+        return []
 
 
 
 def processLineByLine(rootDir,stop_list):
     result=""
+    isann=False
     for i in range (0,len(rootDir)):
         for wordDict in rootDir[i]:
             word=rootDir[i][wordDict][0]
-            POS = rootDir[i][wordDict][1]
-            if isAnaphora(word,POS,stop_list):
+            POS = rootDir[i][wordDict][1]         
+            if rootDir[i][wordDict][5]:#isAnaphora(word,POS,stop_list):
+                ann_num = rootDir[i][wordDict][9]
+                #print(rootDir[i][wordDict][0])
                 if len(rootDir[i][wordDict]) > 5:
-                    gender = rootDir[i][wordDict][5]
+                    gender = rootDir[i][wordDict][7]
                     possibleCand = candidateSelection(rootDir,word, i, gender)
+                    
                     for candidate in possibleCand:
-
-                        result =  result + word + "\t"  + (rootDir[i-1][candidate][0] or "-") +  "\t"+ (rootDir[i-1][candidate][1] or "-") + "\t"+(rootDir[i][wordDict][5] or "-")+ "\t" + (rootDir[i-1][candidate][5] or "-") + "\t" + (rootDir[i][wordDict][6] or "-") + "\t" + (rootDir[i-1][candidate][6] or "-")
+                        can_num=candidate[10]
+                        if(ann_num==can_num and candidate[6]==True):
+                            isann=True
+                        else:
+                            isann=False
+                        result =  result + word + "\t"  + (candidate[0] or "-") +  "\t"+ (candidate[1] or "-") + "\t"+(rootDir[i][wordDict][7] or "-")+ "\t" + (candidate[7] or "-") + "\t" + (rootDir[i][wordDict][8] or "-") + "\t" + (candidate[8] or "-")+ "\t" + str(isann)
                         result = result + "\n"
     return result
 
 def main():
     stop_list = []
-    fp = codecs.open("stop_list.txt", "rb")
-    for line in fp:
-        stop_list.append(line)
-
-    out_file=open("out.txt","wb")
-
-    rootDir= createDict()
-    result= processLineByLine(rootDir,stop_list)
-    out_file.write(result)
-
+    out_file=codecs.open("train_out.txt","w")
+    foldername='C:/USC Course material/CS - 544 NLP/Research Project/Final Idea/Code/workspace/annaphora/trainfiles'
+    filelist=os.listdir(foldername)
+    for x in filelist:
+            print(foldername+'/' + x)
+            rootDir= createDict(foldername+'/' + x)
+            result= processLineByLine(rootDir,stop_list)
+            out_file.write(result)
 if __name__ == '__main__':main()
--- 2.CandidateSelection/CandidateSelection/trainStoryProcessor.py
+++ 2.CandidateSelection/CandidateSelection/trainStoryProcessor.py
@@ -1,25 +1,65 @@
-def createDict():
-    #print("Start")
-    filename = 'story.txt'
+#! /usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+import codecs
+import re
+accepted_annaphora_list=[]
+# with open('acceptedAnaphoraList.txt','r') as f:
+#     accepted_annaphora_list.append(f.readline())
+    
+accepted_annaphora_list = ['मैं','हम','आप' ,'तुम' ,'तू' ,'यह' ,'वह' ,'ये' ,'वे' ,'मुझे' ,'मुझको' ,'हमें' ,'हमको' ,'तुझे' ,'तुझको' ,'तुम्हें' ,'तुमको' ,'आपको' ,'इसे' ,'इसको' ,'इन्हें' ,'इनको' ,'उसे' ,'उसको' ,'उन्हें' ,'उनको' ,'मैंने' ,'हमने' ,'तुने' ,'तुमने' ,'आपने' ,'इसने' ,'इन्होंने' ,'उसने' ,'उन्होंने' ,'मुझसे' ,'हमसे' ,'तुझसे' ,'तुमसे' ,'आपसे' ,'इससे' ,'इनसे' ,'उससे' ,'उनसे' ,'मेरा' ,'मेरी' ,'मेरे' ,'तेरा' ,'तेरी' ,'तेरे' ,'हमारा' ,'हमारी' ,'हमारे' ,'आपका' ,'तुम्हारा' ,'इसका' ,'उसका' ,'इनका' ,'उनका' ,'उनकी' ,'उनके' ,'तुम्हारा' ,'तुम्हारी' ,'तुम्हारे' ,'आपकी' ,'आपके' ,'इसकी' ,'उसकी' ,'इसके' ,'उसके' ,'इनकी' ,'इनके' ,'मुझमे' ,'हममें' ,'तुझमें' ,'आपमें' ,'उसमें' ,'उनमें' ,'इसमें' ,'इनमें' ,'जो' ,'जिसने' ,'जिसे' ,'जिससे' ,'जिसको' ,'जिसका' ,'जिसकी' ,'जिसके' ,'जिसमें' ,'जिन्होनें' ,'जिन्हे' ,'जिनसे' ,'जिनको' ,'जिनका' ,'जिनकी' ,'जिनके' ,'जिनमें' ,'अपना' ,'अपने' ,'अपनी' ,'अपनों' ,'अपनेआप' ,'स्वयं' ,'खुद' ,'कोई' ,'कुछ' ,'सभी' ,'यहां' ,'यहाँ' ,'वहां' ,'वहाँ' ,'जहां' ,'जहाँ' ]
+def createDict(filename):
     rootDir = dict()
     newSentenceDict = dict()
     sentenceCntr = 0
-    
-    with open(filename,'rb') as infile:
+    begin=False
+    ann_num=-1
+    can_num=-1
+    with codecs.open(filename,"r",encoding="utf-8") as infile:
         for line in infile:
-#             print(line)
-            lineTuples = line.split('\t')
+            isAnn=False
+            isCan=False
+            #print(line.encode("utf-8"))
+            lineTuples = line.encode('utf-8').split('\t')
             #print(lineTuples)
-            
-            if lineTuples[0] == '\n':
+            if (lineTuples[0][0] == '#'):
+                continue
+            elif ((lineTuples[0] == '\r\n' or lineTuples[0] == '\n')):
                 rootDir[sentenceCntr] = newSentenceDict
                 sentenceCntr += 1
                 newSentenceDict = dict() 
                 continue
             else:
                 wordAttribList = list()
-                wordAttribList = lineTuples[3:8]
-                
+                m1 = re.search(r"([0-9])+", lineTuples[7])
+                #wordAttribList = lineTuples[3:8]
+                if(lineTuples[5]=='B-NP' and lineTuples[4]=='PRP' and lineTuples[7]!='-' and (lineTuples[3] in accepted_annaphora_list)):
+                    m = re.search(r"\(([0-9])+\)", lineTuples[7])
+                    if(m != None ):
+                        isAnn=True
+                        ann_num=m.group(1)
+                    else:
+                        isAnn=False
+                else:
+                    isAnn=False
+                if(lineTuples[7]!='-'):
+                    if (lineTuples[4]=='NN' or lineTuples[4]=='NNP'): 
+                        isCan=True
+                        can_num = m1.group(0)
+                        begin=False
+                    elif (lineTuples[7].count('(') > lineTuples[7].count(')')): 
+                        can_num = lineTuples[7].strip()[-1:]
+                        begin=True
+                    else:
+                        begin=False    
+                elif (begin):
+                    if (lineTuples[4]=='NN' or lineTuples[4]=='NNP'): 
+                        isCan=True
+                        begin=False
+                else:
+                    isCan=False                    
+                wordAttribList = lineTuples[3:8]      
+                wordAttribList.append(isAnn)
+                wordAttribList.append(isCan)
                 ####### Morph string manipulation starts here #####
                 if len(lineTuples) > 7:
                     morphString = lineTuples[8]
@@ -38,16 +78,18 @@ def createDict():
                         wordAttribList.append(morphKnowledge[3])
                 
                 ##### End of Morph String #####
-                
+                wordAttribList.append(ann_num)
+                wordAttribList.append(can_num)
                 newSentenceDict[lineTuples[2]] = wordAttribList # Added word attributes
 #                 print(newSentenceDict)
         
         rootDir[sentenceCntr] = newSentenceDict     #once last sentence addition      
     return rootDir
-    print(rootDir)
+    
 
 
-if __name__ == "__main__":createDict()
+if __name__ == "__main__":
+    createDict()
 ##################### Notes ########################
 # These are the fields in rootDir #
 # [0] -> Word #
