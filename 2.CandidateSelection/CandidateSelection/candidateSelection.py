ecbbcb2e283fa1cfad885b32655e453fc9747de9
mkampasi <manishakampasi22@gmail.com>
2016-04-23 04:22:29 -0700

    Updated Candidate selection for adding distance feature, regular expressions for annaphors etc

--- 2.CandidateSelection/CandidateSelection/candidateSelection.py
+++ 2.CandidateSelection/CandidateSelection/candidateSelection.py
@@ -13,28 +13,48 @@ from trainStoryProcessor import createDict
 def candidateSelection(rootDir,word,sentenceNum,gender):
     possibleCandidates=[]
     previousLine = {}
-    if sentenceNum > 0:
+    if sentenceNum > 3:
         previousLine["0"] = rootDir[sentenceNum]
         previousLine["1"]=rootDir[sentenceNum - 1]
-        if gender=="m" or gender=="f" or gender=="n" or gender=="fn" :
-            for linevalue in previousLine.itervalues():
-                for wordDict in linevalue.itervalues():
+        previousLine["2"]=rootDir[sentenceNum - 2]
+        previousLine["3"]=rootDir[sentenceNum - 3]
+        previousLine["4"]=rootDir[sentenceNum - 4]
+    if sentenceNum > 2:
+        previousLine["0"] = rootDir[sentenceNum]
+        previousLine["1"]=rootDir[sentenceNum - 1]
+        previousLine["2"]=rootDir[sentenceNum - 2]
+        previousLine["3"]=rootDir[sentenceNum - 3]
+    if sentenceNum > 1:
+        previousLine["0"] = rootDir[sentenceNum]
+        previousLine["1"]=rootDir[sentenceNum - 1]
+        previousLine["2"]=rootDir[sentenceNum - 2]
+    elif sentenceNum > 0:
+        previousLine["0"] = rootDir[sentenceNum]
+        previousLine["1"]=rootDir[sentenceNum - 1]
+#         if gender=="m" or gender=="f" or gender=="n" or gender=="fn" :
+#             for linevalue in previousLine.itervalues():
+#                 for wordDict in linevalue.itervalues():
+#                     if len(wordDict) > 9:
+#                         if ( wordDict[0]!='NULL' and ( wordDict[7] == gender) and (wordDict[1] == "NN" or wordDict[1]=="NNP" or (wordDict[5]==True and wordDict[6]==True and wordDict[0]!=word))):
+#                             possibleCandidates.append(wordDict)
+#         elif gender=="mf":
+#             for linevalue in previousLine.itervalues():
+#                 for wordDict in linevalue.itervalues():
+#                     if len(wordDict) > 9:
+#                         currentWordGender = wordDict[7]
+#                         if( wordDict[0]!='NULL' and (currentWordGender == "m" or currentWordGender == "f" or currentWordGender == "mf") and (wordDict[1]=="NP" or wordDict[1]=="NNP" or (wordDict[5]==True and wordDict[6]==True and wordDict[0]!=word))):
+#                             possibleCandidates.append(wordDict)
+#         else:
+    templist=[]
+    if (len(previousLine)!=0):    
+        for linekey, linevalue in previousLine.iteritems():
+            for wordDict in linevalue.itervalues():
+                if( wordDict[0]!='NULL' and (wordDict[1] == "NN" or wordDict[1]=="NNP" or (wordDict[5]==True and wordDict[6]==True and wordDict[0]!=word))):
                     if len(wordDict) > 9:
-                        if ( ( wordDict[7] == gender) and (wordDict[1] == "NN" or wordDict[1]=="NNP") ):
-                            possibleCandidates.append(wordDict)
-        elif gender=="mf":
-            for linevalue in previousLine.itervalues():
-                for wordDict in linevalue.itervalues():
-                    if len(wordDict) > 9:
-                        currentWordGender = wordDict[7]
-                        if( (currentWordGender == "m" or currentWordGender == "f" or currentWordGender == "mf") and (wordDict[1] or wordDict[1]=="NNP") ):
-                            possibleCandidates.append(wordDict)
-        else:
-            for linevalue in previousLine.itervalues():
-                for wordDict in linevalue.itervalues():
-                    if( wordDict[1] == "NN" or wordDict[1]=="NNP" ):
-                        if len(wordDict) > 9:
-                            possibleCandidates.append(wordDict)
+                        templist = wordDict
+                        templist.append(linekey)
+                        possibleCandidates.append(templist)
+        
             
         return possibleCandidates
     else:
@@ -51,30 +71,34 @@ def processfileLineByLine(rootDir):
     for i in range (0,len(rootDir)):
         for wordDict in rootDir[i]:
             word=rootDir[i][wordDict][0] 
+            isann = rootDir[i][wordDict][5]
             #Check if the function is an annaphora (Should be PRP, B-NP and tagged n the training data)         
-            if rootDir[i][wordDict][5]:
+            if isann:
                 ann_num = rootDir[i][wordDict][9]
                 if len(rootDir[i][wordDict]) > 5:
                     gender = rootDir[i][wordDict][7]
                     #Fetch list of candidate antecedents for the annaphora
                     possibleCand = candidateSelection(rootDir,word, i, gender)                
                     for candidate in possibleCand:
+                        #print(ann_num + " - " + candidate[0] + " - " + str(candidate[5]) + " - " + str(candidate[6]))
                         can_num=candidate[10]
                         #Check if the candidate is the marked/annotated antecedent in the training data
                         if(ann_num==can_num and candidate[6]==True):
-                            isann=True
+                            iscurrCandidate=True
+#                         elif (candidate[5]==True and candidate[6]==True):
+#                              iscurrCandidate=True
                         else:
-                            isann=False
-                        result =  result + word + "\t"  + (candidate[0] or "-") +  "\t"+ (candidate[1] or "-") + "\t"+(rootDir[i][wordDict][7] or "-")+ "\t" + (candidate[7] or "-") + "\t" + (rootDir[i][wordDict][8] or "-") + "\t" + (candidate[8] or "-")+ "\t" + str(isann)
+                            iscurrCandidate=False
+                        result =  result + word + "\t"  + (candidate[0] or "-") +  "\t"+ (candidate[1] or "-") + "\t"+(rootDir[i][wordDict][7] or "-")+ "\t" + (candidate[7] or "-") + "\t" + (rootDir[i][wordDict][8] or "-") + "\t" + (candidate[8] or "-")+ "\t" + candidate[11] + "\t" + str(iscurrCandidate)
                         result = result + "\n"
     result = result + "\n"
     return result
 
 ###############################################################
-#Program starts here
+#Program starts here 
 ############################################################### 
 def main():
-    out_file=codecs.open("train_out.txt","w")
+    out_file=codecs.open("test_out.txt","w")
     foldername='C:/USC Course material/CS - 544 NLP/Research Project/Final Idea/Code/workspace/annaphora/testfiles'
     #Process one file at a time
     filelist=os.listdir(foldername)
--- 2.CandidateSelection/CandidateSelection/trainStoryProcessor.py
+++ 2.CandidateSelection/CandidateSelection/trainStoryProcessor.py
@@ -21,7 +21,7 @@ import re
  
 accepted_annaphora_list=[]
 #List of pronouns and their inflectional forms which are acceptable annaphoras 
-accepted_annaphora_list = ['मैं','हम','आप' ,'तुम' ,'तू' ,'यह' ,'वह' ,'ये' ,'वे' ,'मुझे' ,'मुझको' ,'हमें' ,'हमको' ,'तुझे' ,'तुझको' ,'तुम्हें' ,'तुमको' ,'आपको' ,'इसे' ,'इसको' ,'इन्हें' ,'इनको' ,'उसे' ,'उसको' ,'उन्हें' ,'उन','उनको' ,'मैंने' ,'हमने' ,'तुने' ,'तुमने' ,'आपने' ,'इसने' ,'इन्होंने' ,'उसने' ,'उन्होंने' ,'मुझसे' ,'हमसे' ,'तुझसे' ,'तुमसे' ,'आपसे' ,'इससे' ,'इनसे' ,'उससे' ,'उनसे' ,'मेरा' ,'मेरी' ,'मेरे' ,'तेरा' ,'तेरी' ,'तेरे' ,'हमारा' ,'हमारी' ,'हमारे' ,'आपका' ,'तुम्हारा' ,'इसका' ,'उसका' ,'इनका' ,'उनका' ,'उनकी' ,'उनके' ,'तुम्हारा' ,'तुम्हारी' ,'तुम्हारे' ,'आपकी' ,'आपके' ,'इसकी' ,'उसकी' ,'इसके' ,'उसके' ,'इनकी' ,'इनके' ,'मुझमे' ,'हममें' ,'तुझमें' ,'आपमें' ,'उसमें' ,'उनमें' ,'इसमें' ,'इनमें' ,'जो' ,'जिसने' ,'जिसे' ,'जिससे' ,'जिसको' ,'जिसका' ,'जिसकी' ,'जिसके' ,'जिसमें' ,'जिन्होनें' ,'जिन्हे' ,'जिनसे' ,'जिनको' ,'जिनका' ,'जिनकी' ,'जिनके' ,'जिनमें' ,'अपना' ,'अपने' ,'अपनी' ,'अपनों' ,'अपनेआप' ,'स्वयं' ,'खुद' ,'कोई' ,'कुछ' ,'सभी' ,'यहां' ,'यहाँ' ,'वहां' ,'वहाँ' ,'जहां' ,'जहाँ' ]
+accepted_annaphora_list = ['जिन्हें','मैं','हम','आप' ,'तुम' ,'तू' ,'यह' ,'वह' ,'ये' ,'वे' ,'मुझे' ,'मुझको' ,'हमें' ,'हमको' ,'तुझे' ,'तुझको' ,'तुम्हें' ,'तुमको' ,'आपको' ,'इसे' ,'इसको' ,'इन्हें' ,'इनको' ,'उसे' ,'उस','उसको' ,'उन्हें' ,'उन','उनको' ,'मैंने' ,'हमने' ,'तुने' ,'तुमने' ,'आपने' ,'इसने' ,'इन्होंने' ,'उसने' ,'उन्होंने' ,'मुझसे' ,'हमसे' ,'तुझसे' ,'तुमसे' ,'आपसे' ,'इससे' ,'इनसे' ,'उससे' ,'उनसे' ,'मेरा' ,'मेरी' ,'मेरे' ,'तेरा' ,'तेरी' ,'तेरे' ,'हमारा' ,'हमारी' ,'हमारे' ,'आपका' ,'तुम्हारा' ,'इसका' ,'उसका' ,'इनका' ,'उनका' ,'उनकी' ,'उनके' ,'तुम्हारा' ,'तुम्हारी' ,'तुम्हारे' ,'आपकी' ,'आपके' ,'इसकी' ,'उसकी' ,'इसके' ,'उसके' ,'इनकी' ,'इनके' ,'मुझमे' ,'हममें' ,'तुझमें' ,'आपमें' ,'उसमें' ,'उनमें' ,'इसमें' ,'इनमें' ,'जो' ,'जिसने' ,'जिसे' ,'जिससे' ,'जिसको' ,'जिसका' ,'जिसकी' ,'जिसके' ,'जिसमें' ,'जिन्होनें' ,'जिन्हे' ,'जिनसे' ,'जिनको' ,'जिनका' ,'जिनकी' ,'जिनके' ,'जिनमें' ,'अपना' ,'अपने' ,'अपनी' ,'अपनों' ,'अपनेआप' ,'स्वयं' ,'खुद' ,'कोई' ,'कुछ' ,'सभी' ,'यहां' ,'यहाँ' ,'वहां' ,'वहाँ' ,'जहां' ,'जहाँ' ]
 
 def createDict(filename):
     rootDir = dict()
@@ -46,10 +46,13 @@ def createDict(filename):
                 continue
             else:
                 wordAttribList = list()
-                m1 = re.search(r"([0-9])+", lineTuples[7])
+                m1 = re.search(r"([0-9]+)", lineTuples[7]) #Case (x)
+                m2 = re.search(r"\(([0-9]+)", lineTuples[7]) #Case (x
+                m3 = re.search(r"\(([0-9]+)\)[|]\(([0-9]+)", lineTuples[7]) #Case (x)|(y
+                m4 = re.search(r"\(([0-9]+)\)[|]\(([0-9]+)\)", lineTuples[7]) #Case (x)|(y)
                 #wordAttribList = lineTuples[3:8]
-                if(lineTuples[5]=='B-NP' and lineTuples[4]=='PRP' and lineTuples[7]!='-' and (lineTuples[3] in accepted_annaphora_list)):
-                    m = re.search(r"\(([0-9])+\)", lineTuples[7])
+                if(lineTuples[5]=='B-NP' and lineTuples[4]=='PRP' and lineTuples[7]!='-'):
+                    m = re.search(r"\(([0-9]+)\)", lineTuples[7])
                     if(m != None ):
                         isAnn=True
                         ann_num=m.group(1)
@@ -60,11 +63,22 @@ def createDict(filename):
                 if(lineTuples[7]!='-'):
                     if (lineTuples[4]=='NN' or lineTuples[4]=='NNP'): 
                         isCan=True
-                        can_num = m1.group(0)
+                        can_num = m1.group(1)
                         begin=False
-                    elif (lineTuples[7].count('(') > lineTuples[7].count(')')): 
-                        can_num = lineTuples[7].strip()[-1:]
+                    #cover case (x
+                    elif (lineTuples[7].count('(') > lineTuples[7].count(')') and lineTuples[7].count('(')==1): 
+                        can_num = m2.group(1)
                         begin=True
+                    #cover case (x)|(y
+                    elif (lineTuples[7].count('(') > lineTuples[7].count(')') and lineTuples[7].count('(')!=1):
+                        isCan=True
+                        can_num = m3.group(2)
+                        begin=False 
+                    #cover case (x)|(y)
+                    elif (lineTuples[7].count('(') == lineTuples[7].count(')') and lineTuples[7].count('(')>1):
+                        isCan=True
+                        can_num = m4.group(2)
+                        begin=False 
                     else:
                         begin=False    
                 elif (begin):
@@ -94,11 +108,18 @@ def createDict(filename):
                         wordAttribList.append(morphKnowledge[3])
                 
                 ##### End of Morph String #####
-                wordAttribList.append(ann_num)
-                wordAttribList.append(can_num)
+                if isAnn:
+                    wordAttribList.append(ann_num)
+                else:
+                    wordAttribList.append(0)
+                if isCan:
+                    wordAttribList.append(can_num)
+                else:
+                    wordAttribList.append(0)
                 newSentenceDict[lineTuples[2]] = wordAttribList # Added word attributes
         
         rootDir[sentenceCntr] = newSentenceDict     #once last sentence addition      
+    print(rootDir)
     return rootDir
     
 
